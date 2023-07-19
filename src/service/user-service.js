c23523dd5f265a6ac5a632b2d53216d0ee45b3ea
mujibur-rohman <muji.official06@gmail.com>
2023-07-19 11:49:26 +0700

    login user

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,6 +1,10 @@
+import { v4 as uuid } from "uuid";
 import { prismaCLient } from "../app/database.js";
 import { ResponseError } from "../error/response-error.js";
-import { registerUserValidation } from "../validation/user-validation.js";
+import {
+  loginUserValidation,
+  registerUserValidation,
+} from "../validation/user-validation.js";
 import { validate } from "../validation/validation.js";
 import bcrypt from "bcrypt";
 
@@ -28,6 +32,42 @@ const register = async (request) => {
   });
 };
 
+const login = async (request) => {
+  const loginRequest = validate(loginUserValidation, request);
+
+  const user = await prismaCLient.user.findUnique({
+    where: { username: loginRequest.username },
+    select: { username: true, password: true },
+  });
+  if (!user) {
+    throw new ResponseError(403, "Username or password wrong");
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+    loginRequest.password,
+    user.password
+  );
+
+  if (!isPasswordValid) {
+    throw new ResponseError(403, "Username or password wrong");
+  }
+
+  const token = uuid().toString();
+
+  return prismaCLient.user.update({
+    data: {
+      token,
+    },
+    where: {
+      username: user.username,
+    },
+    select: {
+      token: true,
+    },
+  });
+};
+
 export default {
   register,
+  login,
 };
--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,7 +1,10 @@
 import { ResponseError } from "../error/response-error.js";
 
 const validate = (schema, request) => {
-  const result = schema.validate(request);
+  const result = schema.validate(request, {
+    abortEarly: false, // untuk memvalidasi semuanya
+    allowUnknown: false, // untuk tidak menyetujui adanya body yang diluar validasi
+  });
   if (result.error) {
     throw new ResponseError(400, result.error.message);
   } else {
